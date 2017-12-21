470198792bcabb326a1c1cc72dce8431e646b95e
sdp009 <shubhamcja177@gmail.com>
2017-12-21 12:42:21 -0500

    basic template

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+let win;
+function createWindow () {
+  // Create the browser window.
+  let {width, height} = require('electron').screen.getPrimaryDisplay().size;
+  win = new BrowserWindow({width: width, height: height});
+
+  win.loadURL(`file://${__dirname}/dist/index.html`)
+  //// uncomment below to open the DevTools.
+  // win.webContents.openDevTools()
+  // Event when the window is closed.
+  win.on('closed', function () {
+    win = null
+  })
+}
+// Create window on electron intialization
+app.on('ready', createWindow)
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', function () {
+  // macOS specific close process
+  if (win === null) {
+    createWindow()
+  }
+})
