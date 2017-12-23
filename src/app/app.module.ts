e7e9b1436d65c734ce0148f97c47c7b44f468cf0
sdp009 <shubhamcja177@gmail.com>
2017-12-22 20:19:16 -0500

    add css-overflow & login route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,17 @@ import { SignUpComponent } from './sign-up/sign-up.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'signup',
+        component: SignUpComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
